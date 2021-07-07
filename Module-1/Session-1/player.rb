125a6cfa112cd0fc8ed1be42e5940f0cab01dd96
marrosandybaguss <marrosandybaguss@gmail.com>
2021-07-07 17:43:01 +0700

    rev player

--- Module-1/Session-1/player.rb
+++ Module-1/Session-1/player.rb
@@ -14,17 +14,7 @@ class Player
   
   def attack(other_person)
     puts "#{@name} attacks #{other_person.name} with #{@attack_demage} demage."
-
-    if other_person.name == "Sandy"
-      if !deflect()
-        other_person.take_demage(@attack_demage)
-      else
-        puts "#{other_person.name} deflects the attack."
-      end
-    else
-      other_person.take_demage(@attack_demage)
-    end
-    
+    other_person.take_demage(@attack_demage)
   end
   
   def take_demage(demage)
