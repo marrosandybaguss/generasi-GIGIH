59799fff8e9e330a80b6e4316786824b3add458d
marrosandybaguss <marrosandybaguss@gmail.com>
2021-07-28 19:52:04 +0700

    refactor increment array of integer

--- Module-4/Session-2/spec/increment_integer_spec.rb
+++ Module-4/Session-2/spec/increment_integer_spec.rb
@@ -1,27 +1,27 @@
-require_relative "../src/increment_integer.rb"
+require_relative "../src/increment_array_integer.rb"
 
-RSpec.describe IncrementInteger do
+RSpec.describe IncrementArrayInteger do
 
     it '561 to 562' do
-        increment_integer = IncrementInteger.new
+        increment_array_integer = IncrementArrayInteger.new
 
-        result = increment_integer.increment([5, 6, 1])
+        result = increment_array_integer.increment([5, 6, 1])
 
         expect(result).to eq([5, 6, 2])
     end
 
     it '39 to 40' do
-        increment_integer = IncrementInteger.new
+        increment_array_integer = IncrementArrayInteger.new
 
-        result = increment_integer.increment([3, 9])
+        result = increment_array_integer.increment([3, 9])
 
         expect(result).to eq([4, 0])
     end
 
     it '99 to 100' do
-        increment_integer = IncrementInteger.new
+        increment_array_integer = IncrementArrayInteger.new
 
-        result = increment_integer.increment([3, 9])
+        result = increment_array_integer.increment([3, 9])
 
         expect(result).to eq([4, 0])
     end
