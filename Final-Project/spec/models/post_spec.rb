29f6a499da9da082a38e3c4cbf5798495f223117
marrosandybaguss <marrosandybaguss@gmail.com>
2021-08-19 13:57:03 +0700

    fail valid post

---
+++ Final-Project/spec/models/post_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../../models/post'
+
+describe Post do
+  describe '#valid?' do
+    context 'when initialized with valid input' do
+      it 'should return true' do
+        post = Post.new(5, 'This is #post')
+        expect(post.valid?).to eq(true)
+      end
+    end
+
+    context 'when initialized with invalid input' do
+      it 'should return false' do
+        post = Post.new(5)
+        expect(post.valid?).to eq(false)
+      end
+    end
+  end
+end
