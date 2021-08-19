3e60bb5bf7f71be65e0f0d1ddddd559f55f181e3
marrosandybaguss <marrosandybaguss@gmail.com>
2021-08-19 18:34:50 +0700

    pass insert_post - unit test

--- Final-Project/spec/models/post_spec.rb
+++ Final-Project/spec/models/post_spec.rb
@@ -36,18 +36,31 @@ describe Post do
     end
 
     context 'when execute get all data post in one day' do
-        it 'should return all of data post in one day' do
-          stub_client = double
-          hastagh = '#post'
-          stub_query = 'SELECT * FROM posts WHERE reg_date > DATE_SUB(CURDATE(), INTERVAL 1 DAY)'
-          posts = [{ "id": 5, "post": 'This is #post' }]
-  
-          allow(Mysql2::Client).to receive(:new).and_return(stub_client)
-          expect(stub_client).to receive(:query).with(stub_query).and_return(posts)
-  
-          result = Post.select_post_24hours
-          expect(result).not_to be_nil
-        end
+      it 'should return all of data post in one day' do
+        stub_client = double
+        stub_query = 'SELECT * FROM posts WHERE reg_date > DATE_SUB(CURDATE(), INTERVAL 1 DAY)'
+        posts = [{ "id": 5, "post": 'This is #post' }]
+
+        allow(Mysql2::Client).to receive(:new).and_return(stub_client)
+        expect(stub_client).to receive(:query).with(stub_query).and_return(posts)
+
+        result = Post.select_post_24hours
+        expect(result).not_to be_nil
       end
+    end
+  end
+
+  describe '#insert data' do
+    context 'when executed' do
+      it 'should save data' do
+        stub_client = double
+        stub_query = "INSERT INTO posts (post) VALUES ('This is #post')"
+        post = Post.new(5, 'This is #post')
+
+        allow(Mysql2::Client).to receive(:new).and_return(stub_client)
+        expect(stub_client).to receive(:query).with(stub_query)
+        post.insert_post
+      end
+    end
   end
 end
