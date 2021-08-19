5325b0d80e10ce7a7f6f84a4ba49a5a64a9902e6
marrosandybaguss <marrosandybaguss@gmail.com>
2021-08-20 01:15:31 +0700

    unit test fail create_post - controllers

--- Final-Project/controllers/post_controller.rb
+++ Final-Project/controllers/post_controller.rb
@@ -5,7 +5,7 @@ require_relative '../models/post'
 
 class PostController
   def create_post(params)
-    posts = Post.new(nil, params['post'])
+    posts = Post.new(nil, params[:post])
     posts.insert_post
   end
 
--- Final-Project/spec/controllers/post_controller_spec.rb
+++ Final-Project/spec/controllers/post_controller_spec.rb
@@ -16,4 +16,26 @@ describe PostController do
       expect(response).to eq(posts.to_json)
     end
   end
+
+  describe '#create' do
+    context 'with valid parameter' do
+      it 'create a new post' do
+        post_controller = PostController.new
+        post_params = { id: nil, post: 'this is #post' }
+
+        stub_model = double
+        mock_post_return = double
+        controller_result = post_controller.create_post(post_params)
+
+        allow(Post).to receive(:new).with(post_params).and_return(stub_model)
+        allow(stub_model).to receive(:insert_post).and_return(mock_post_return)
+        allow(mock_post_return).to receive(:each)
+
+        expect(controller_result).to eq({
+                                          'status' => 200,
+                                          'message' => 'Success'
+                                        })
+      end
+    end
+  end
 end
