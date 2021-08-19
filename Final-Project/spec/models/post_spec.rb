7614ed7cdf25f485b85c70ce31fa6857595488a5
marrosandybaguss <marrosandybaguss@gmail.com>
2021-08-19 14:56:04 +0700

    pass select_filter_posts - unit test

--- Final-Project/spec/models/post_spec.rb
+++ Final-Project/spec/models/post_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative '../../models/post'
 
 describe Post do
@@ -13,6 +15,23 @@ describe Post do
       it 'should return false' do
         post = Post.new(5, nil)
         expect(post.valid?).to eq(false)
+      end
+    end
+  end
+
+  describe '#get data' do
+    context 'when execute get all data post apropiate hastagh' do
+      it 'should return all of data post apropiate hastagh' do
+        stub_client = double
+        hastagh = '#post'
+        stub_query = "select * from posts where post like '%#{hastagh}%'"
+        posts = [{ "id": 5, "post": 'This is #post' }]
+
+        allow(Mysql2::Client).to receive(:new).and_return(stub_client)
+        expect(stub_client).to receive(:query).with(stub_query).and_return(posts)
+
+        result = Post.select_filter_posts(hastagh)
+        expect(result).not_to be_nil
       end
     end
   end
